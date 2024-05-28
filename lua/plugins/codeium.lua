ea8dcc656c787b5b4ed390650b45688f5b334777
mahadi hasan <mahadi.dev@outlook.com>
2024-05-29 02:04:20 +0600

    refactor: codeium config

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -6,5 +6,8 @@ return {
     vim.keymap.set("i", "<C-Right>", function()
       return vim.fn["codeium#Accept"]()
     end, { expr = true, silent = true })
+    vim.keymap.set("i", "<C-Left>", function()
+      return vim.fn["codeium#Chat"]()
+    end, { expr = true, silent = false })
   end,
 }
