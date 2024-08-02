39ca8fae69a21cf9217a4865ae15db0a63114e26
mahadi hasan <mahadi.dev@outlook.com>
2024-08-03 01:09:04 +0600

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+local opt = vim.opt
+
+opt.laststatus = 0
+opt.clipboard = "unnamedplus"
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.shiftround = true
+opt.showmode = true
+opt.smartindent = true
+opt.relativenumber = true
