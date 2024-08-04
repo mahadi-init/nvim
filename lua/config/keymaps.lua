62cf99cd05fe89a75576ca5f31426c302be69b8f
mahadi hasan <mahadi.dev@outlook.com>
2024-08-04 13:59:34 +0600

    refactor: add window spilit keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,13 +2,17 @@ local builtin = require('telescope.builtin')
 
 -- telescope
 -- vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-vim.keymap.set('n', '<leader><leader>', ":Telescope frecency workspace=CWD<CR>", {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader><leader>', ":Telescope frecency workspace=CWD<CR>", {}) -- open telescope
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})                             -- live grep
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})                               -- view buffers
 
 -- oil
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" }) -- oil open parent dir
 
 -- basic
-vim.keymap.set("n", "<C-s>", ":w<CR>", {})
-vim.keymap.set("n", "<leader>q", ":q<CR>", {})
+vim.keymap.set("n", "<C-s>", ":w<CR>", {})     -- save file
+vim.keymap.set("n", "<leader>q", ":q<CR>", {}) -- quit neovim
+
+-- split
+vim.keymap.set("n", "<C-v>", ":vsplit<CR>", {})
+vim.keymap.set("n", "<C-x>", ":close<CR>", {})
