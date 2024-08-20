899e64faf463178ea37c6006207010873c2acc64
mahadi hasan <mahadi.dev@outlook.com>
2024-08-21 00:39:37 +0600

    add indent-blankline plugin

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  ---@module "ibl"
+  opts = {},
+  config = function()
+    require("ibl").setup({
+      scope = { enabled = false },
+    })
+  end
+}
