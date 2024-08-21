fa330c95ae230e327b6df12b780083c05f80ea8a
mahadi hasan <mahadi.dev@outlook.com>
2024-08-21 15:07:05 +0600

    add new plugin changes

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -6,7 +6,45 @@ return {
     require('mini.tabline').setup()
     require('mini.pairs').setup()
     require('mini.indentscope').setup()
-    -- require('mini.files').setup()
-    -- require('mini.animate').setup()
+    require('mini.files').setup({
+      content = {
+        filter = nil,
+        prefix = nil,
+        sort = nil,
+      },
+
+      mappings = {
+        close       = '<ESC>',
+        go_in       = 'l',
+        go_in_plus  = '<CR>',
+        go_out      = 'h',
+        go_out_plus = 'H',
+        reset       = ';',
+        reveal_cwd  = '',
+        show_help   = 'g?',
+        synchronize = 's',
+        trim_left   = '<',
+        trim_right  = '>',
+      },
+
+      options = {
+        permanent_delete = true,
+        use_as_default_explorer = true,
+      },
+
+      windows = {
+        max_number = math.huge,
+        preview = false,
+        width_focus = 50,
+        width_nofocus = 15,
+        width_preview = 25,
+      },
+    })
+
+    require('mini.animate').setup({
+      scroll = {
+        enable = false
+      }
+    })
   end
 }
