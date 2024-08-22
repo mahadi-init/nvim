f102b95a3d3794f44ecb63451bd66c204b155a73
mahadi hasan <mahadi.dev@outlook.com>
2024-08-22 18:11:27 +0600

    remove plugin

--- lua/plugins/multiple-cursor.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "brenton-leighton/multiple-cursors.nvim",
-  version = "*",
-  opts = {},
-  keys = {
-    { "<C-Up>",   "<CMD>MultipleCursorsAddUp<CR>",   mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-    { "<C-Down>", "<CMD>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
-  },
-}
