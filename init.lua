f67eb61840df6c668f0b6315bb80743816e93344
mahadi hasan <mahadi.dev@outlook.com>
2024-09-15 02:51:14 +0600

    minimal vscode for config

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
-require 'config.remove-padding'
-require 'config.lazy'
-require 'config.options'
-require 'config.cmpconfig'
-require 'config.lspconfig'
-require 'config.autocmds'
-require 'config.keymaps'
+vim.opt.clipboard = 'unnamedplus'
+vim.keymap.set('n', '<ESC>', '<CMD>nohlsearch<CR>') -- escape and remove search query
