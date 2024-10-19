b301dc97770a17a2895b933f7fd7925d1a3e6dad
mahadi-hasan <init.mahadi@gmail.com>
2024-10-19 14:03:35 +0600

    change theme config

--- lua/plugins/bamboo.lua
+++ lua/plugins/bamboo.lua
@@ -5,48 +5,39 @@ return {
   priority = 1000,
   config = function()
     require('bamboo').setup {
-      -- Lua
-      require('bamboo').setup {
-        -- Main options --
-        style = 'multiplex', -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
-        toggle_style_key = nil, -- Keybind to toggle theme style. Leave it nil to disable it, or set it to a string, e.g. "<leader>ts"
-        toggle_style_list = { 'vulgaris', 'multiplex', 'light' }, -- List of styles to toggle between
-        transparent = true, -- Show/hide background
-        dim_inactive = false, -- Dim inactive windows/buffers
-        term_colors = true, -- Change terminal color as per the selected theme style
-        ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-        cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+      style = 'multiplex', -- Choose between 'vulgaris' (regular), 'multiplex' (greener), and 'light'
+      toggle_style_list = { 'vulgaris', 'multiplex', 'light' }, -- List of styles to toggle between
+      transparent = true, -- Show/hide background
+      dim_inactive = false, -- Dim inactive windows/buffers
+      term_colors = true, -- Change terminal color as per the selected theme style
+      ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+      cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+      code_style = {
+        comments = { italic = false },
+        conditionals = { italic = false },
+        keywords = {},
+        functions = {},
+        namespaces = { italic = false },
+        parameters = { italic = false },
+        strings = {},
+        variables = {},
+      },
 
-        -- Change code style ---
-        -- Options are anything that can be passed to the `vim.api.nvim_set_hl` table
-        -- You can also configure styles with a string, e.g. keywords = 'italic,bold'
-        code_style = {
-          comments = { italic = false },
-          conditionals = { italic = false },
-          keywords = {},
-          functions = {},
-          namespaces = { italic = false },
-          parameters = { italic = false },
-          strings = {},
-          variables = {},
-        },
+      -- Lualine options --
+      lualine = {
+        transparent = true, -- lualine center bar transparency
+      },
+      colors = {},
+      highlights = {
+        -- make comments blend nicely with background, similar to other color schemes
+        ['@comment'] = { fg = '$grey' },
+      },
 
-        -- Lualine options --
-        lualine = {
-          transparent = true, -- lualine center bar transparency
-        },
-        colors = {},
-        highlights = {
-          -- make comments blend nicely with background, similar to other color schemes
-          ['@comment'] = { fg = '$grey' },
-        },
-
-        -- Plugins Config --
-        diagnostics = {
-          darker = true, -- darker colors for diagnostic
-          undercurl = true, -- use undercurl instead of underline for diagnostics
-          background = true, -- use background color for virtual text
-        },
+      -- Plugins Config --
+      diagnostics = {
+        darker = true, -- darker colors for diagnostic
+        undercurl = true, -- use undercurl instead of underline for diagnostics
+        background = true, -- use background color for virtual text
       },
     }
     require('bamboo').load()
