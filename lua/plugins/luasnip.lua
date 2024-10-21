1ab7a2a0ae2bdf9df165d1b19de050935b707ce2
mahadi-hasan <init.mahadi@gmail.com>
2024-10-21 20:16:35 +0600

    add new custom snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -8,4 +8,72 @@ return {
     'rafamadriz/friendly-snippets',
     'saadparwaiz1/cmp_luasnip',
   },
+  config = function()
+    local ls = require 'luasnip'
+    local s = ls.snippet
+    local i = ls.insert_node
+    local t = ls.text_node
+
+    -- React functional component snippet
+    ls.add_snippets('typescript', {
+      s('rfc', {
+        t { "import React from 'react';", '', 'export default function ' },
+        i(1, 'ComponentName'),
+        t { '() {', '\treturn (' },
+        t { '', '\t\t<div>' },
+        i(2, 'content'),
+        t { '</div>', '\t);', '};' },
+      }),
+    })
+
+    -- Function declaration snippet
+    ls.add_snippets('typescript', {
+      s('fn', {
+        t 'function ',
+        i(1, 'name'),
+        t '(',
+        i(2, 'params'),
+        t { ') {', '\t' },
+        i(3, 'body'),
+        t { '', '}' },
+      }),
+    })
+
+    -- Function declaration snippet
+    ls.add_snippets('typescript', {
+      s('afn', {
+        t 'export async function ',
+        i(1, 'name'),
+        t '(',
+        i(2, 'params'),
+        t { ') {', '\t' },
+        i(3, 'body'),
+        t { '', '}' },
+      }),
+    })
+
+    -- Exported function snippet
+    ls.add_snippets('typescript', {
+      s('efn', {
+        t 'export function ',
+        i(1, 'functionName'),
+        t { '() {', '\t' },
+        i(2, '// your code here'),
+        t { '', '}' },
+      }),
+    })
+
+    -- Arrow function snippet
+    ls.add_snippets('typescript', {
+      s('af', {
+        t 'const ',
+        i(1, 'name'),
+        t ' = (',
+        i(2, 'params'),
+        t { ') => {', '\t' },
+        i(3, 'body'),
+        t { '', '};' },
+      }),
+    })
+  end,
 }
