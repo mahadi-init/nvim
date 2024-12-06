053f11ecefdb1b3a2a58ffd72d9ea85f3d7c9575
mahadi-init <init.mahadi@gmail.com>
2024-12-07 02:33:54 +0600

    add new flash plugin

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -591,4 +591,26 @@ return {
       vim.cmd [[nnoremap \ :Neotree reveal<cr>]]
     end,
   },
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    keys = {
+      {
+        's',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').jump()
+        end,
+        desc = 'Flash',
+      },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+      },
+    },
+  },
 }
