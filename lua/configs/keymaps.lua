26d3d869b62f4b233d466874b4faa8f966bfeff2
mahadi-init <init.mahadi@gmail.com>
2024-12-11 23:48:56 +0600

    change : to <CMD>

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,13 +1,13 @@
 local telescope = require 'telescope.builtin'
 
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', ':w<CR>') -- save file
-vim.keymap.set('n', '<leader>q', ':q<CR>') -- quit neovim
-vim.keymap.set('n', '<ESC>', ':nohlsearch<CR>') -- escape and remove search query
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s>', '<CMD>w<CR>') -- save file
+vim.keymap.set('n', '<leader>q', '<CMD>q<CR>') -- quit neovim
+vim.keymap.set('n', '<ESC>', '<CMD>nohlsearch<CR>') -- escape and remove search query
 vim.keymap.set('n', '<C-j>', '<C-d>zz') -- scroll down
 vim.keymap.set('n', '<C-k>', '<C-u>zz') -- scroll up
-vim.keymap.set('n', '<C-v>', ':vsplit<CR>') -- vertical split
-vim.keymap.set('n', '<C-Right>', ':bnext<CR>') -- next buffer
-vim.keymap.set('n', '<C-Left>', ':bprevious<CR>') -- previous buffer
+vim.keymap.set('n', '<C-v>', '<CMD>vsplit<CR>') -- vertical split
+vim.keymap.set('n', '<C-Right>', '<CMD>bnext<CR>') -- next buffer
+vim.keymap.set('n', '<C-Left>', '<CMD>bprevious<CR>') -- previous buffer
 
 -- telescope
 vim.keymap.set('n', '<leader><leader>', telescope.find_files, { desc = 'find files' }) -- find files
@@ -18,7 +18,7 @@ vim.keymap.set('n', '<leader>fg', telescope.git_files, { desc = 'git files' }) -
 vim.keymap.set('n', '<leader>fs', telescope.git_status, { desc = 'git status' }) -- git status
 
 -- file tree
-vim.keymap.set('n', '<leader>e', ':Neotree filesystem toggle<CR>', { desc = 'neo tree open' }) -- file tree
+vim.keymap.set('n', '<leader>e', '<CMD>Neotree filesystem toggle<CR>', { desc = 'neo tree open' }) -- file tree
 
 -- session
 vim.keymap.set('n', '<leader>ls', function()
@@ -26,19 +26,19 @@ vim.keymap.set('n', '<leader>ls', function()
 end)
 
 -- lazygit
-vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = 'LazyGit' }) -- file tree
+vim.keymap.set('n', '<leader>gg', '<CMD>LazyGit<CR>', { desc = 'LazyGit' }) -- file tree
 
 -- buf delete
-vim.keymap.set('n', '<C-x>', ':lua Snacks.bufdelete()<CR>', { desc = 'Delete buffer' })
+vim.keymap.set('n', '<C-x>', '<CMD>lua Snacks.bufdelete()<CR>', { desc = 'Delete buffer' })
 
 -- show current line blame
-vim.api.nvim_set_keymap('n', '<leader>gb', ':lua Snacks.git.blame_line()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gb', '<CMD>lua Snacks.git.blame_line()<CR>', { noremap = true, silent = true })
 
 -- spectre
-vim.keymap.set('n', '<C-t>', '<cmd>lua require("spectre").toggle()<CR>', {
+vim.keymap.set('n', '<C-t>', '<CMD>lua require("spectre").toggle()<CR>', {
   desc = 'Toggle Spectre',
 })
-vim.keymap.set({ 'n', 'v' }, '<C-f>', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+vim.keymap.set({ 'n', 'v' }, '<C-f>', '<CMD>lua require("spectre").open_file_search({select_word=true})<CR>', {
   desc = 'Search on current file',
 })
 
