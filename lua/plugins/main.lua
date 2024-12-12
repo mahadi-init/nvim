a9464087c89875ca1c4c2e1eab1ba534a9427d86
mahadi-init <init.mahadi@gmail.com>
2024-12-12 17:54:02 +0600

    new plugin and add readme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -706,5 +706,11 @@ return {
         },
       })
     end
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    event = "VeryLazy",
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    opts = {},
   }
 }
