c4c7481b802a97fb0f268db12a5a66c1ec6689fb
mahadi-init <init.mahadi@gmail.com>
2024-12-12 15:54:48 +0600

    add prisma

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -410,8 +410,8 @@ return {
         enable_git_status = true,
         enable_diagnostics = true,
         open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
-        sort_case_insensitive = false, -- used when sorting files and directories in the tree
-        sort_function = nil, -- use a custom function for sorting files and directories in the tree
+        sort_case_insensitive = false,                                     -- used when sorting files and directories in the tree
+        sort_function = nil,                                               -- use a custom function for sorting files and directories in the tree
         -- sort_function = function (a,b)
         --       if a.type == b.type then
         --           return a.path > b.path
@@ -469,10 +469,10 @@ return {
           git_status = {
             symbols = {
               -- Change type
-              added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
+              added = '',    -- or "✚", but this is redundant info if you use git_status_colors on the name
               modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
-              deleted = '', -- this can only be used in the git_status source
-              renamed = '', -- this can only be used in the git_status source
+              deleted = '',  -- this can only be used in the git_status source
+              renamed = '',  -- this can only be used in the git_status source
               -- Status type
               untracked = '',
               ignored = '',
@@ -543,11 +543,11 @@ return {
             },
           },
           follow_current_file = {
-            enabled = true, -- This will find and focus the file in the active buffer every time
+            enabled = true,                   -- This will find and focus the file in the active buffer every time
             --               -- the current file is changed while the tree is open.
-            leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            leave_dirs_open = false,          -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
           },
-          group_empty_dirs = false, -- when true, empty folders will be grouped together
+          group_empty_dirs = false,           -- when true, empty folders will be grouped together
           hijack_netrw_behavior = 'disabled', -- netrw disabled, opening a directory opens neo-tree
           -- in whatever position is specified in window.position
           -- "open_current",  -- netrw disabled, opening a directory opens within the
@@ -560,11 +560,11 @@ return {
         },
         buffers = {
           follow_current_file = {
-            enabled = true, -- This will find and focus the file in the active buffer every time
+            enabled = true,          -- This will find and focus the file in the active buffer every time
             --              -- the current file is changed while the tree is open.
             leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
           },
-          group_empty_dirs = true, -- when true, empty folders will be grouped together
+          group_empty_dirs = true,   -- when true, empty folders will be grouped together
           show_unloaded = true,
           window = {},
         },
@@ -677,5 +677,12 @@ return {
       vim.keymap.set('n', '<space>ff', ':Telescope file_browser path=%:p:h select_buffer=true<CR>')
     end,
   },
-  { 'sindrets/diffview.nvim' },
+  {
+    'sindrets/diffview.nvim',
+    event = "VeryLazy"
+  },
+  {
+    'prisma/vim-prisma',
+    event = "VeryLazy"
+  }
 }
