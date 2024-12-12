87cd6e8568ac82c79ee412521c5cda2ff8728734
mahadi-init <init.mahadi@gmail.com>
2024-12-12 11:06:40 +0600

    removed added plugin

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -678,45 +678,4 @@ return {
     end,
   },
   { 'sindrets/diffview.nvim' },
-  {
-    'fgheng/winbar.nvim',
-    config = function()
-      require('winbar').setup {
-        enabled = true,
-        show_file_path = true,
-        show_symbols = true,
-
-        colors = {
-          path = '#FA8072', -- You can customize colors like #c946fd
-          file_name = '#2bb0b2',
-          symbols = '',
-        },
-
-        icons = {
-          file_icon_default = '',
-          seperator = '',
-          editor_state = '●',
-          lock_icon = '',
-        },
-
-        exclude_filetype = {
-          'help',
-          'startify',
-          'dashboard',
-          'packer',
-          'neogitstatus',
-          'NvimTree',
-          'Trouble',
-          'alpha',
-          'lir',
-          'Outline',
-          'spectre_panel',
-          'toggleterm',
-          'qf',
-          'neo-tree',
-          'lazygit',
-        },
-      }
-    end,
-  },
 }
