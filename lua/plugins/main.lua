55e0c3375660504b295c6a7a5dd0dd6fa14c8f30
mahadi-init <init.mahadi@gmail.com>
2024-12-13 01:51:52 +0600

    remove plugin

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -698,11 +698,5 @@ return {
         },
       })
     end
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    event = "VeryLazy",
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    opts = {},
   }
 }
