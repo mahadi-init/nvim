ed2859ead74194628acd4679d0a2ba88a158866e
mahadi-init <init.mahadi@gmail.com>
2024-12-21 22:37:46 +0600

    add multiple cursor feature

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -201,8 +201,8 @@ return {
             i = {
               -- ['<C-c>'] = 'Close',
               ['<CR>'] = 'Confirm',
-              ['<Up>'] = 'HistoryPrev',
-              ['<Down>'] = 'HistoryNext',
+              -- ['<Up>'] = 'HistoryPrev',
+              -- ['<Down>'] = 'HistoryNext',
             },
           },
 
@@ -614,6 +614,58 @@ return {
     event = "VeryLazy",
     config = function()
       require('beacon').setup()
+    end
+  },
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    event = "VeryLazy",
+    config = function()
+      local mc = require("multicursor-nvim")
+
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "v" }, "<up>",
+        function() mc.lineAddCursor(-1) end)
+      set({ "n", "v" }, "<down>",
+        function() mc.lineAddCursor(1) end)
+
+      -- Add or skip adding a new cursor by matching word/selection
+      set({ "n", "v" }, "<leader>n",
+        function() mc.matchAddCursor(1) end)
+      set({ "n", "v" }, "<leader>s",
+        function() mc.matchSkipCursor(1) end)
+      set({ "n", "v" }, "<leader>N",
+        function() mc.matchAddCursor(-1) end)
+      set({ "n", "v" }, "<leader>S",
+        function() mc.matchSkipCursor(-1) end)
+
+      -- Add all matches in the document
+      set({ "n", "v" }, "<leader>A", mc.matchAllAddCursors)
+
+      -- Add and remove cursors with control + left click.
+      set("n", "<c-leftmouse>", mc.handleMouse)
+
+      set("n", "<esc>", function()
+        if not mc.cursorsEnabled() then
+          mc.enableCursors()
+        elseif mc.hasCursors() then
+          mc.clearCursors()
+        else
+        end
+      end)
+
+      -- Customize how cursors look.
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { link = "Cursor" })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
     end
   }
 }
