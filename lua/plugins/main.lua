d2eb6baa8c03d9e346038538da4516482edd638d
mahadi-init <init.mahadi@gmail.com>
2024-12-23 03:02:00 +0600

    remove italic from theme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -36,7 +36,7 @@ return {
     'sainnhe/everforest',
     priority = 1000,
     config = function()
-      vim.g.everforest_enable_italic = true
+      vim.g.everforest_enable_italic = false
       vim.g.everforest_background = "hard"
       vim.cmd.colorscheme('everforest')
     end
