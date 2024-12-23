5a4c769a67da1bab3588858311eae0c1c870a3b3
mahadi-init <init.mahadi@gmail.com>
2024-12-23 13:08:35 +0600

    change theme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -33,38 +33,6 @@ return {
     end,
   },
   {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("gruvbox").setup({
-        terminal_colors = true,
-        undercurl = true,
-        underline = true,
-        bold = true,
-        italic = {
-          strings = false,
-          emphasis = false,
-          comments = false,
-          operators = false,
-          folds = false
-        },
-        strikethrough = true,
-        invert_selection = false,
-        invert_signs = false,
-        invert_tabline = false,
-        invert_intend_guides = false,
-        inverse = true, -- invert background for search, diffs, statuslines and errors
-        contrast = "",  -- can be "hard", "soft" or empty string
-        palette_overrides = {},
-        overrides = {},
-        dim_inactive = false,
-        transparent_mode = false,
-      })
-      vim.cmd("colorscheme gruvbox")
-    end
-  },
-  {
     'echasnovski/mini.tabline',
     version = false,
     config = function()
