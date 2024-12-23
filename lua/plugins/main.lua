9fedebd62e6aeb9d870ac0ac762b9e93b0824786
mahadi-init <init.mahadi@gmail.com>
2024-12-23 12:03:09 +0600

    change theme

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -33,12 +33,35 @@ return {
     end,
   },
   {
-    'sainnhe/everforest',
+    "ellisonleao/gruvbox.nvim",
     priority = 1000,
+    opts = {},
     config = function()
-      vim.g.everforest_enable_italic = false
-      vim.g.everforest_background = "hard"
-      vim.cmd.colorscheme('everforest')
+      require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "",  -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+      vim.cmd("colorscheme gruvbox")
     end
   },
   {
