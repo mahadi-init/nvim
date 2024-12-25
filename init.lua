4c27018925be48d71a67e75834b5f8e658611f3b
mahadi-init <init.mahadi@gmail.com>
2024-12-26 02:37:46 +0600

    add new plugin and code cleanup

--- init.lua
+++ init.lua
@@ -1,49 +1,5 @@
--- Set up lazy.nvim path and repository
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-
--- Clone lazy.nvim if not already present
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local out = vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    lazyrepo,
-    '--branch=stable',
-    lazypath,
-  }
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out,                            'WarningMsg' },
-      { '\nPress any key to exit...' },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-
--- Prepend lazy.nvim to runtime path
-vim.opt.rtp:prepend(lazypath)
-
--- Set leader keys
-vim.g.mapleader = ' '
-vim.g.maplocalleader = '\\'
-
--- Lazy.nvim setup
-require('lazy').setup {
-  spec = {
-    { import = 'plugins' },
-  },
-  install = { colorscheme = { 'gruvbox' } },
-  checker = { enabled = true },
-}
---> LAZY NVIM ENDS
-
---> IMPORT OTHER PACKAGES
 require 'configs.options'
 require 'configs.autocmds'
+require 'configs.lazy'
 require 'configs.keymaps'
--- require 'configs.cmp'
 require 'configs.lsp'
--- IMPORT PACKEAGE END HERE
---
+++ lua/configs/lazy.lua
@@ -0,0 +1,40 @@
+-- Set up lazy.nvim path and repository
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+
+-- Clone lazy.nvim if not already present
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    lazyrepo,
+    '--branch=stable',
+    lazypath,
+  }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out,                            'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+-- Prepend lazy.nvim to runtime path
+vim.opt.rtp:prepend(lazypath)
+
+-- Set leader keys
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
+
+-- Lazy.nvim setup
+require('lazy').setup {
+  spec = {
+    { import = 'plugins' },
+  },
+  install = { colorscheme = { 'gruvbox-material' } },
+  checker = { enabled = true },
+}
