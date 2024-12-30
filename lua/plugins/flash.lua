3cfc408b9a460dcff59c75de22a0cff6723bea7d
mahadi-init <init.mahadi@gmail.com>
2024-12-31 02:41:19 +0600

    remove flash keymaps

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,22 +2,22 @@ return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   opts = {},
-  keys = {
-    {
-      'S',
-      mode = { 'n', 'x', 'o' },
-      function()
-        require('flash').jump()
-      end,
-      desc = 'Flash',
-    },
-    -- {
-    --   'S',
-    --   mode = { 'n', 'x', 'o' },
-    --   function()
-    --     require('flash').treesitter()
-    --   end,
-    --   desc = 'Flash Treesitter',
-    -- },
-  },
+  -- keys = {
+  --   {
+  --     'S',
+  --     mode = { 'n', 'x', 'o' },
+  --     function()
+  --       require('flash').jump()
+  --     end,
+  --     desc = 'Flash',
+  --   },
+  --   {
+  --     'S',
+  --     mode = { 'n', 'x', 'o' },
+  --     function()
+  --       require('flash').treesitter()
+  --     end,
+  --     desc = 'Flash Treesitter',
+  --   },
+  -- },
 }
