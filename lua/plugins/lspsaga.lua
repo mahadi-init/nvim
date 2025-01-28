1a3f343d6800a63e6a5521f3bf1c168a5f1fe0e7
mahadi-init <init.mahadi@gmail.com>
2025-01-28 17:16:06 +0600

    add one more level for lspsaga

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -19,7 +19,7 @@ return {
         separator = ' › ',
         hide_keyword = false,
         show_file = true,
-        folder_level = 1,
+        folder_level = 2,
         color_mode = true,
         delay = 300,
       },
