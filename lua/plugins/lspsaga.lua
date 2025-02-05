5216b74efed040e2654ca6b8867a0d3513b58aa3
mahadi-init <init.mahadi@gmail.com>
2025-02-05 10:08:25 +0600

    change lspsaga config

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -2,20 +2,20 @@ return {
   'nvimdev/lspsaga.nvim',
   event = 'VeryLazy',
   dependencies = {
-    'nvim-treesitter/nvim-treesitter', -- optional
-    'nvim-tree/nvim-web-devicons', -- optional
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
     require('lspsaga').setup {
       lightbulb = {
-        enable = false, -- Disable the lightbulb feature
-        enable_in_insert = false, -- Disable in insert mode if applicable
-        sign = true, -- Keep sign if needed
+        enable = false,
+        enable_in_insert = false,
+        sign = false,
         sign_priority = 40,
-        virtual_text = false, -- Disable virtual text if needed
+        virtual_text = false,
       },
       symbol_in_winbar = {
-        enable = false, -- Set this to false to disable breadcrumbs
+        enable = true,
         separator = ' › ',
         hide_keyword = false,
         show_file = true,
