1c9e14bfae66427183a5b33fd3a2336cd9e5a714
mahadi-init <init.mahadi@gmail.com>
2025-02-19 13:32:30 +0600

    stop auto update check in neovim plugin

--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
-      { out,                            'WarningMsg' },
+      { out, 'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
     vim.fn.getchar()
@@ -35,6 +35,6 @@ require('lazy').setup {
   spec = {
     { import = 'plugins' },
   },
-  install = { colorscheme = { 'gruvbox-material' } },
-  checker = { enabled = true },
+  install = { colorscheme = { 'catppuccin' } },
+  checker = { enabled = false },
 }
