8ed2ac8916bb05f1edfe08c6f07f73ca8d052956
mahadi-init <init.mahadi@gmail.com>
2025-03-19 09:50:11 +0600

    add go lsp and formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,6 +5,9 @@ return {
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = {
+          'gopls',
+        },
         javascript = {
           'prettierd',
         },
