9b18a4493da649971c88db5f01d18baa7d5fea70
mahadi-init <init.mahadi@gmail.com>
2025-04-10 11:45:09 +0600

    remove deprecated api

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -27,11 +27,14 @@ return {
     },
   },
   config = function()
-    -- If you want icons for diagnostic errors, you'll need to define them somewhere:
-    vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticSignError' })
-    vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
-    vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
-    vim.fn.sign_define('DiagnosticSignHint', { text = '󰌵', texthl = 'DiagnosticSignHint' })
+    vim.diagnostic.config {
+      signs = {
+        Error = { text = ' ', texthl = 'DiagnosticSignError' },
+        Warning = { text = ' ', texthl = 'DiagnosticSignWarning' },
+        Hint = { text = '󰌵', texthl = 'DiagnosticSignHint' },
+        Info = { text = ' ', texthl = 'DiagnosticSignInfo' },
+      },
+    }
 
     require('neo-tree').setup {
       close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
@@ -41,13 +44,6 @@ return {
       open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- when opening files, do not use windows containing these filetypes or buftypes
       sort_case_insensitive = false, -- used when sorting files and directories in the tree
       sort_function = nil, -- use a custom function for sorting files and directories in the tree
-      -- sort_function = function (a,b)
-      --       if a.type == b.type then
-      --           return a.path > b.path
-      --       else
-      --           return a.type > b.type
-      --       end
-      --   end , -- this sorts files and directories descendantly
       default_component_configs = {
         container = {
           enable_character_fade = true,
@@ -55,12 +51,10 @@ return {
         indent = {
           indent_size = 2,
           padding = 1, -- extra padding on left hand side
-          -- indent guides
           with_markers = true,
           indent_marker = '│',
           last_indent_marker = '└',
           highlight = 'NeoTreeIndentMarker',
-          -- expander config, needed for nesting files
           with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
           expander_collapsed = '',
           expander_expanded = '',
@@ -131,9 +125,6 @@ return {
           enabled = false,
         },
       },
-      -- A list of functions, each representing a global custom command
-      -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
-      -- see `:h neo-tree-custom-commands-global`
       commands = {},
       window = {
         position = 'left',
