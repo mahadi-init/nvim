4270a5ea8c67d217448040c9fc511704407c0b52
mahadi-init <init.mahadi@gmail.com>
2025-04-10 11:12:39 +0600

    add sorting algorithm for neovim

---
+++ lua/plugins/all-recent.lua
@@ -0,0 +1,9 @@
+return {
+  'prochri/telescope-all-recent.nvim',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'kkharji/sqlite.lua',
+    'stevearc/dressing.nvim',
+  },
+  opts = {},
+}
