16f61a368be1746fb5728f9ad2a31b32ce5babec
mahadi-init <init.mahadi@gmail.com>
2025-04-17 11:47:24 +0600

    move from biome to prettier

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,27 +2,20 @@ return {
   'stevearc/conform.nvim',
   event = 'VeryLazy',
   config = function()
-    local js_ts_formatters = {
-      'biome',
-      'prettier',
-      'prettierd',
-      stop_after_first = true,
-    }
-
     local prettier_formatters = {
-      'prettier',
       'prettierd',
+      'prettier',
       stop_after_first = true,
     }
 
     require('conform').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = js_ts_formatters,
-        typescript = js_ts_formatters,
-        typescriptreact = js_ts_formatters,
-        html = js_ts_formatters,
-        css = js_ts_formatters,
+        javascript = prettier_formatters,
+        typescript = prettier_formatters,
+        typescriptreact = prettier_formatters,
+        html = prettier_formatters,
+        css = prettier_formatters,
         json = prettier_formatters,
       },
       format_after_save = {
