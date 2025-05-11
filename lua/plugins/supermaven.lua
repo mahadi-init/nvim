9f6c1aaa8695938989a50eea4b245040c51e55ea
mahadi-init <init.mahadi@gmail.com>
2025-05-11 14:19:01 +0600

    add supermaven

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,12 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<M-]>",
+      },
+      ignore_filetypes = { ["neo-tree"] = true, ["DressingInput"] = true }
+    })
+  end,
+}
