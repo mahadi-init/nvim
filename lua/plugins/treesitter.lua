f2ba6d182946de8df54fd0ee541c0efd704c4cea
mahadi-init <init.mahadi@gmail.com>
2025-05-17 12:21:22 +0600

    add treesitter changes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   event = 'VeryLazy',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      highlight = {
+        enable = true,
+      },
+    }
+  end,
 }
