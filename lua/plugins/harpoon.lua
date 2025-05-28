1f5bdb7bd10144f7802b451ec96f87fae1ead729
mahadi-init <init.mahadi@gmail.com>
2025-05-28 11:54:16 +0600

    remove bufferline

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,39 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  event = 'VeryLazy',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end)
+
+    vim.keymap.set('n', '<C-e>', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    vim.keymap.set('n', '<C-b>', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<C-m>', function()
+      harpoon:list():select(3)
+    end)
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<C-p>', function()
+      harpoon:list():prev()
+    end)
+    vim.keymap.set('n', '<C-n>', function()
+      harpoon:list():next()
+    end)
+  end,
+}
