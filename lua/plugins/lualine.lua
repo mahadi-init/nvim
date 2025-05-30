8909ef4246e047cae8898cd34d65e65121379909
mahadi-init <init.mahadi@gmail.com>
2025-05-30 22:03:49 +0600

    add changes

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -25,6 +25,10 @@ return {
         lualine_a = { 'mode' },
         lualine_b = {
           'branch',
+          function()
+            local relative_filepath = vim.fn.expand '%:.'
+            return relative_filepath
+          end,
         },
         lualine_c = {
           'diagnostics',
