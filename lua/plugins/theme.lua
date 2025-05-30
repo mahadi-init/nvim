e2789fdaa70513116a28c4167c566b0dcb21f58b
mahadi-init <init.mahadi@gmail.com>
2025-05-30 14:42:17 +0600

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,53 +1,10 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'sainnhe/gruvbox-material',
+  lazy = false,
   priority = 1000,
   config = function()
-    require('catppuccin').setup {
-      flavour = 'mocha',
-      background = { light = 'latte', dark = 'mocha' },
-      transparent_background = true,
-      show_end_of_buffer = false,
-      term_colors = false,
-      dim_inactive = {
-        enabled = false,
-        shade = 'dark',
-        percentage = 0.15,
-      },
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
-      styles = {
-        comments = {},
-        conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-      },
-      color_overrides = {},
-      custom_highlights = {},
-      default_integrations = true,
-      integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-          enabled = true,
-          indentscope_color = '',
-        },
-      },
-    }
-
-    -- setup must be called before loading
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+    vim.g.gruvbox_material_background = "hard"
+    vim.g.gruvbox_material_foreground = "material"
+    vim.cmd.colorscheme('gruvbox-material')
+  end
 }
