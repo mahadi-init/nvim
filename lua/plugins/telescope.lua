b1bc688a2f41b57d858965075bf0e8c0a97362b6
mahadi-init <init.mahadi@gmail.com>
2025-06-17 16:31:56 +0600

    add marks with telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -36,9 +36,10 @@ return {
 
     -- Key mappings should be outside the setup function
     vim.keymap.set('n', '<leader><leader>', telescope.find_files, { desc = 'find files' }) -- find files
-    vim.keymap.set('n', '<C-g>', telescope.git_status, { desc = 'git status' }) -- git status
-    vim.keymap.set('n', '<leader>fw', telescope.live_grep, { desc = 'find words' }) -- live grep
+    vim.keymap.set('n', '<C-g>', telescope.git_status, { desc = 'git status' })            -- git status
+    vim.keymap.set('n', '<leader>fw', telescope.live_grep, { desc = 'find words' })        -- live grep
     vim.keymap.set('n', '<C-b>', telescope.buffers, { desc = 'Find buffer' })
     vim.keymap.set('n', '<leader>ft', telescope.current_buffer_fuzzy_find, { desc = 'find text current buffer' })
+    vim.keymap.set('n', '<leader>fm', telescope.marks, { desc = 'marks' })
   end,
 }
