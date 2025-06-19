ef47f47ffee3370adb732e59c3788d4650a11192
mahadi-init <init.mahadi@gmail.com>
2025-06-19 14:13:50 +0600

    windsurf added

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,6 +2,24 @@ return {
   'nvim-lualine/lualine.nvim',
   event = 'VeryLazy',
   config = function()
+    local function custom_status()
+      local status = require('codeium.virtual_text').status()
+
+      if status.state == 'idle' then
+        return ' '
+      end
+
+      if status.state == 'waiting' then
+        return " "
+      end
+
+      if status.state == 'completions' and status.total > 0 then
+        return string.format('%d/%d', status.current, status.total)
+      end
+
+      return ' 0 '
+    end
+
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -34,6 +52,7 @@ return {
           'diagnostics',
         },
         lualine_x = {
+          custom_status,
           function()
             local progress = require('lsp-progress').progress()
             if progress:match 'LSP$' then
