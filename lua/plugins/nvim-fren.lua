94c9a0bd15a49d2f8d23f5d323e0596bce253c6a
mahadi-init <init.mahadi@gmail.com>
2025-07-04 05:35:27 +0600

    change nvim fren keymaps

--- lua/plugins/nvim-fren.lua
+++ lua/plugins/nvim-fren.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
   },
   config = function()
-    Key("n", "<leader>e", "<CMD>:Fern . -drawer -toggle<CR>")
+    Key("n", "<leader>e", "<CMD>:Fern . -reveal=% -drawer -toggle<CR>")
     Key("n", "-", "<CMD>:Fern . -reveal=%<CR>")
   end
 }
