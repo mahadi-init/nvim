a80a8c46603bbc77c34b7bee358a5cbf39f098d8
mahadi-init <init.mahadi@gmail.com>
2025-07-05 01:53:39 +0600

    update vim-fern

--- lua/plugins/nvim-fren.lua
+++ lua/plugins/nvim-fren.lua
@@ -2,9 +2,12 @@ return {
   "lambdalisue/vim-fern",
   event = "VeryLazy",
   dependencies = {
+    "TheLeoP/fern-renderer-web-devicons.nvim",
   },
   config = function()
-    Key("n", "<leader>e", "<CMD>:Fern . -reveal=% -drawer -toggle<CR>")
+    vim.g["fern#renderer"] = "nvim-web-devicons"
+
+    Key("n", "<leader>e", "<CMD>:Fern . -drawer -toggle<CR>")
     Key("n", "-", "<CMD>:Fern . -reveal=%<CR>")
   end
 }
