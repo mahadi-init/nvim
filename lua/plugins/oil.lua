cc1919b4dfb35cbffd41dc8d8ad3c0e43bc091e4
mahadi-init <init.mahadi@gmail.com>
2025-07-05 03:18:33 +0600

    add keymap changes

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,28 @@ return {
   "stevearc/oil.nvim",
   event = "VeryLazy",
   config = function()
-    require("oil").setup()
+    require("oil").setup({
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
+      cleanup_delay_ms = 2000,
+      lsp_file_methods = {
+        enabled = true,
+        timeout_ms = 500,
+        autosave_changes = true,
+      },
+      watch_for_changes = true,
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["<C-p>"] = "actions.preview",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = { "actions.parent", mode = "n" },
+        ["_"] = { "actions.open_cwd", mode = "n" },
+        ["g."] = { "actions.toggle_hidden", mode = "n" },
+      },
+      use_default_keymaps = false,
+    })
+
     Key("n", "<leader>e", function()
       vim.ui.input({
         prompt = "Open Oil at path: ",
