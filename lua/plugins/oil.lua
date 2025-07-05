a43acdd76cb1a97f7801b155cbc35756e728a420
mahadi-init <init.mahadi@gmail.com>
2025-07-05 10:44:00 +0600

    update neovim file manager config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,7 +2,10 @@ return {
   "stevearc/oil.nvim",
   event = "VeryLazy",
   config = function()
-    require("oil").setup({
+    local oil = require("oil")
+    local fzf = require("fzf-lua")
+
+    oil.setup({
       default_file_explorer = true,
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = false,
@@ -25,11 +28,24 @@ return {
     })
 
     Key("n", "<leader>e", function()
-      vim.fn.inputsave()
-      local path = vim.fn.input("Oil: Open Path » ", vim.fn.getcwd() .. "/", "file")
-      vim.fn.inputrestore()
-      if path ~= "" then vim.cmd.Oil(path) end
-    end, { desc = "Open Oil file explorer" })
+      fzf.files({
+        prompt = "Oil: Select folder » ",
+        cwd = vim.fn.getcwd(),
+        cmd = "fd --type d --hidden --exclude .git",
+        file_icons = false,
+        color_icons = false,
+        git_icons = false,
+        actions = {
+          ["default"] = function(selected)
+            if selected and selected[1] then
+              local clean_path = selected[1]:match("^%s*(.-)%s*$")
+              clean_path = vim.fn.fnamemodify(clean_path, ":p")
+              oil.open(clean_path)
+            end
+          end,
+        },
+      })
+    end, { desc = "Search & Open Folder in Oil (fzf-lua)" })
 
     Key("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
   end
