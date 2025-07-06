a013ebf43ca6e8194e1e464cab6ab74155698614
mahadi-init <init.mahadi@gmail.com>
2025-07-06 22:40:26 +0600

    add gitignore in file search

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,11 @@
 return {
-  "stevearc/oil.nvim",
-  event = "VeryLazy",
+  'stevearc/oil.nvim',
+  event = 'VeryLazy',
   config = function()
-    local oil = require("oil")
-    local fzf = require("fzf-lua")
+    local oil = require 'oil'
+    local fzf = require 'fzf-lua'
 
-    oil.setup({
+    oil.setup {
       default_file_explorer = true,
       skip_confirm_for_simple_edits = true,
       prompt_save_on_select_new_entry = false,
@@ -17,22 +17,21 @@ return {
       },
       watch_for_changes = true,
       keymaps = {
-        ["<CR>"] = "actions.select",
-        ["<C-p>"] = "actions.preview",
-        ["<C-l>"] = "actions.refresh",
-        ["<C-c>"] = { "actions.close", mode = "n" },
-        ["-"] = { "actions.parent", mode = "n" },
-        ["_"] = { "actions.open_cwd", mode = "n" },
-        ["g."] = { "actions.toggle_hidden", mode = "n" },
+        ['<CR>'] = 'actions.select',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-l>'] = 'actions.refresh',
+        ['<C-c>'] = { 'actions.close', mode = 'n' },
+        ['-'] = { 'actions.parent', mode = 'n' },
+        ['_'] = { 'actions.open_cwd', mode = 'n' },
+        ['g.'] = { 'actions.toggle_hidden', mode = 'n' },
       },
       use_default_keymaps = false,
-    })
+    }
 
-
-    Key("n", "<leader>e", function()
+    Key('n', '<leader>e', function()
       local cwd = vim.fn.getcwd()
-      local fd_cmd = "fd --type d --hidden --exclude .git"
-      local home = "󰣉 root/"
+      local fd_cmd = 'fd --type d --hidden --exclude .git'
+      local home = '󰣉 root/'
 
       -- Run the `fd` command to get folder list
       local fd_handle = io.popen(fd_cmd)
@@ -47,32 +46,36 @@ return {
       -- Prepend "Home" (mapped to current root)
       table.insert(results, 1, home)
 
-      require("fzf-lua").fzf_exec(results, {
-        prompt = "Select folder » ",
+      require('fzf-lua').fzf_exec(results, {
+        prompt = 'Select folder » ',
         cwd = cwd,
         file_icons = false,
         color_icons = false,
         git_icons = false,
         previewer = false,
+        file_ignore_patterns = {
+          '^.git/',
+          'bun.lock',
+          '^.gitignore$',
+        },
         actions = {
-          ["default"] = function(selected)
+          ['default'] = function(selected)
             if selected and selected[1] then
-              local selected_path = selected[1]:match("^%s*(.-)%s*$")
+              local selected_path = selected[1]:match '^%s*(.-)%s*$'
 
               -- If "Home" selected, go to cwd (root)
               if selected_path == home then
-                selected_path = "."
+                selected_path = '.'
               end
 
-              local clean_path = vim.fn.fnamemodify(selected_path, ":p")
-              require("oil").open(clean_path)
+              local clean_path = vim.fn.fnamemodify(selected_path, ':p')
+              require('oil').open(clean_path)
             end
           end,
         },
       })
-    end, { desc = "Search & Open Folder in Oil (fzf-lua)" })
+    end, { desc = 'Search & Open Folder in Oil (fzf-lua)' })
 
-
-    Key("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+    Key('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end,
 }
