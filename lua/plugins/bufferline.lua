9c982efbe08ce94295ba0a5627891e0320007a16
mahadi-init <init.mahadi@gmail.com>
2025-07-09 12:52:43 +0600

    remove supermaven

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,6 @@
 return {
   'akinsho/bufferline.nvim',
   version = '*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     vim.opt.termguicolors = true
     local bufferline = require 'bufferline'
