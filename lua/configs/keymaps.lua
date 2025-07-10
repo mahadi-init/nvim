132c34ff915a975851fe7c089be34fd67032c798
mahadi-init <init.mahadi@gmail.com>
2025-07-10 11:48:04 +0600

    add new keymap for line movement

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -16,6 +16,11 @@ Key('n', '<M-j>', '<CMD>cnext<CR>', { desc = 'next quickfix' })
 -- resize window
 Key('n', '=', [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
 -- Key('n', '-', [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+--
+vim.keymap.set('n', '<C-Up>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('v', '<C-Up>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+vim.keymap.set('n', '<C-Down>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('v', '<C-Down>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
 
 -- session
 Key('n', '<leader>ls', function()
