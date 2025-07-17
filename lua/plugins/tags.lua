78f049bb878daa249a4092df584444f31242fb84
mahadi-init <init.mahadi@gmail.com>
2025-07-17 15:07:35 +0600

    changes

--- lua/plugins/tags.lua
+++ lua/plugins/tags.lua
@@ -4,10 +4,10 @@ return {
   lazy = true,
   cmd = { 'TagAdd', 'TagRemove', 'TagShow', 'TagClear' },
   keys = {
-    { '<C-c>', '<cmd>TagAdd<CR>' },
+    { '<C-a>', '<cmd>TagAdd<CR>' },
     { '<C-t>', '<cmd>TagShow<CR>' },
+    { '<C-c>', '<cmd>TagClear<CR>' },
     { '<leader>tr', '<cmd>TagRemove<CR>' },
-    { '<leader>tc', '<cmd>TagClear<CR>' },
   },
   config = function()
     require('tags').setup()
