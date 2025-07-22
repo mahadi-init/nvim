8867f1653217aaa43dfa7e69310b73950c8a0a3f
mahadi-init <init.mahadi@gmail.com>
2025-07-22 10:07:29 +0600

    changes

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,18 +12,16 @@ return {
     picker = {},
   },
   config = function(_, opts)
-    local map = vim.keymap.set
-
     require('snacks').setup(opts)
 
     -- Keymaps
-    map('n', '<leader>fd', function()
+    Key('n', '<leader>fd', function()
       require('snacks').picker.diagnostics()
     end, { desc = 'Find diagnostics/errors' })
-    map('v', '<C-f>', function()
+    Key('v', '<C-f>', function()
       require('snacks').picker.grep_word()
     end, { desc = 'Find word under selection' })
-    map('n', '<C-x>', function()
+    Key('n', '<C-x>', function()
       require('snacks').bufdelete()
     end, { desc = 'Delete buffer' })
   end,
