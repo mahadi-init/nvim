e5e6a12baf89d88bbca2163ec5ef5088e957f489
mahadi-init <init.mahadi@gmail.com>
2025-07-30 06:10:01 +0600

    Remove: markdown plugin

--- lua/plugins/markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  event = "VeryLazy",
-  after = { 'nvim-treesitter' },
-  config = function()
-    require('render-markdown').setup({})
-  end,
-}
